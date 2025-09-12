b47012a8e9ca43b2ca029a8576434b904283467c
mauro <maurobilardo02@gmail.com>
2025-09-12 17:15:52 +0200

    mi ero scordato in tutto questo l'utilizzo dell'async

--- script/script.js
+++ script/script.js
@@ -25,36 +25,32 @@ btn2.addEventListener('click', () =>{
     loadMoviePage(page);
 })
 
-//FUNZIONE PER CARICARE LE PAGINE (INIZIALMENTE PARTE DA 1)
-function loadMoviePage(page = 1){
+//FUNZIONE ASINCRONA PER CARICARE LE PAGINE (INIZIALMENTE PARTE DA 1)
+async function loadMoviePage(page = 1){
   // MANDA LA RICHIETA AL SERVER MOVIE POPULAR CON IL VALORE DELLA PAGINA VARIABILE
-  fetch(`https://api.themoviedb.org/3/movie/popular?page=${page}`, options)
-  //CONVERTE LA RESPONSE IN UN JSON  
+  await fetch(`https://api.themoviedb.org/3/movie/popular?page=${page}`, options) 
   .then(res => res.json())
-  //PER OGNI DATA CHE RIENTRA IN .MOVIES STAMPA 1 ELEMENTO
+  //CONVERTE LA RESPONSE IN UN JSON PER OGNI DATA CHE RIENTRA IN .MOVIES STAMPA 1 ELEMENTO
     .then(data => {
       const container = document.querySelector('.movies');
       container.innerHTML = '';
       //CICLA I RISULTATI LIMITANDO A 10 STAMPE
-      data.results.slice(0,10).forEach(movie => {
+      data.results.slice(0,18).forEach(movie => {
         fetch(`https://api.themoviedb.org/3/movie/${movie.id}`, options)
           .then(response => response.json())
           .then(details =>{
             //CREA I DIV PER DARGLI LO SPAZIO NECESSARIO
             const movieDiv = document.createElement('div');
             movieDiv.classList.add('movie');
-
             //RECUPERA L'IMMAGINE DEL FILM E IN ALTERNATIVA STAMPA L'ALT
             const image = details.poster_path
             ? `https://image.tmdb.org/t/p/w500${details.poster_path}`
             : 'https://via.placeholder.com/500x750?text=No+Image';
-            
             //NE DEFINISCE I NOMI PRENDENDONE IL TITOLO DA DETAILS
             movieDiv.innerHTML = `
               <img src="${image}" alt="${details.title}" />
               <h3>${details.title}</h3>
             `;
-            
             //APPENDE IL RISULTATO DI TUTTO STO MACELLO A movieDiv
             container.appendChild(movieDiv);
           });
